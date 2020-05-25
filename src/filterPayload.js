8613fba3889ed1c1f201479a0ca7d02f86f7f67b
Chinmay Pandhare <pandharechinmay882@gmail.com>
2020-05-25 20:00:13 +0800

    Refactor handleRequest and filterPayload to use the reliable GHWH Spec, prepare for adding support for GHWH push event

--- src/filterPayload.js
+++ src/filterPayload.js
@@ -1,25 +1,28 @@
-module.exports = payload => ({
-	sha: payload.sha,
-	target_url: payload.target_url,
-	avatar_url: payload.avatar_url,
-	context: payload.context,
-	state: payload.state,
-	description: payload.description,
-	commit: {
-		message: payload.commit.commit.message,
-		url: payload.commit.url,
-	},
-	sender: {
-		login: payload.sender.login,
-		avatar_url: payload.sender.avatar_url,
-		html_url: payload.sender.html_url,
-		type: payload.sender.type,
-	},
-	branches: payload.branches.map(({name}) => name),
-	created_at: payload.created_at,
-	updated_at: payload.updated_at,
-	repository: {
-		full_name: payload.repository.full_name,
-		html_url: payload.repository.html_url,
-	},
-});
+const filterStatusPayload = payload => {
+	return new Promise((resolve, reject) => {
+		try {
+			resolve({
+				sha: payload.sha,
+				name: payload.repository.full_name,
+				target_url: payload.target_url,
+				state: payload.state,
+				description: payload.description,
+				sender: {
+					login: payload.sender.login,
+					avatar_url: payload.sender.avatar_url,
+					html_url: payload.sender.html_url,
+					type: payload.sender.type,
+				},
+				branches: payload.branches.map(({name}) => name),
+				repository: {
+					full_name: payload.repository.full_name,
+					html_url: payload.repository.html_url,
+				},
+			});
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+module.exports = {filterStatusPayload};
--- src/handleRequest.js
+++ src/handleRequest.js
@@ -1,80 +1,79 @@
 const fs = require('fs');
 
 const run = require('./run.js');
-const filterPayload = require('./filterPayload.js');
-
-const STATES = ['pending', 'success', 'failure', 'error'];
+const {filterStatusPayload} = require('./filterPayload.js');
 
 const sendErrorMessage = (message, res, name, db) => {
 	if (name && db.has(name).value()) {
 		db.set(`${name}.message`, message)
-			.set(`${name}.state`, 'undeployed')
+			.set(`${name}.state`, 'not-deployed')
 			.write();
 	}
 	res.status(400).send({message});
 };
 
-module.exports = (req, res, db) => {
-	if (
-		STATES.indexOf(req.body.state) !== -1 &&
-		typeof req.body.name === 'string'
-	) {
-		// Is a state event
-		const payload = req.body;
-		if (db.has(payload.name).value()) {
-			// Check if a local directory is linked
-			let isDir = false;
-			try {
-				isDir = fs.lstatSync(db.get(payload.name).value().path).isDirectory();
-			} catch (err) {
-				// Local dir no longer exists
-			}
-			if (isDir) {
-				// Valid local dir
-				if (!db.get(`${payload.name}.events`).value()) {
-					db.set(`${payload.name}.events`, []).write();
+const deploy = (payload, db) => {
+	const {name, sha} = payload;
+	run(db.get(name).value().path, sha)
+		.then(outputs => {
+			db.set(`${name}.state`, 'deployed')
+				.set(`${name}.message`, 'Deployed Successfully')
+				.set(`${name}.outputs`, outputs)
+				.write();
+		})
+		.catch(outputs => {
+			db.set(`${name}.state`, 'not-deployed')
+				.set(
+					`${name}.message`,
+					`Failed to deploy. ${
+						outputs[outputs.length - 1].command
+					} returned a non-zero exit code.`
+				)
+				.set(`${name}.outputs`, outputs)
+				.write();
+		});
+};
+
+module.exports = async (req, res, db) => {
+	const event = req.headers['x-github-event'];
+	if (event === 'status') {
+		try {
+			const payload = await filterStatusPayload(req.body);
+			const {name, state, description} = payload;
+			if (db.has(name).value()) {
+				let isDirValid = false;
+				try {
+					isDirValid = fs.lstatSync(db.get(name).value().path).isDirectory();
+				} catch (err) {}
+				if (isDirValid) {
+					if (!db.get(`${name}.events`).value()) {
+						db.set(`${name}.events`, []).write();
+					}
+					db.set(`${name}.state`, state)
+						.set(`${name}.message`, description)
+						.get(`${name}.events`)
+						.unshift(payload)
+						.write();
+					res.send({
+						status: 'OK',
+						state,
+						description,
+					});
+					if (state === 'success') deploy(payload, db);
+				} else {
+					const message = 'Invalid local directory linked to ghwh-deploy';
+					sendErrorMessage(message, res, name, db);
 				}
-				db.set(`${payload.name}.state`, payload.state)
-					.set(`${payload.name}.message`, payload.description)
-					.get(`${payload.name}.events`)
-					.unshift(filterPayload(payload))
-					.write();
-				res.send({
-					status: 'OK',
-					state: payload.state,
-					description: payload.description,
-				});
-				if (payload.state === 'success')
-					run(db.get(payload.name).value().path, payload.sha)
-						.then(outputs => {
-							db.set(`${payload.name}.state`, 'deployed')
-								.set(`${payload.name}.message`, 'Deployed Successfully')
-								.set(`${payload.name}.outputs`, outputs)
-								.write();
-						})
-						.catch(outputs => {
-							db.set(`${payload.name}.state`, 'not-deployed')
-								.set(
-									`${payload.name}.message`,
-									`Failed to deploy. ${
-										outputs[outputs.length - 1].command
-									} returned a non-zero exit code.`
-								)
-								.set(`${payload.name}.outputs`, outputs)
-								.write();
-						});
 			} else {
-				// Local path not a dir
-				const message = 'Invalid local directory linked to ghwh-deploy';
-				sendErrorMessage(message, res, payload.name, db);
+				const message = `No local path found for ${name}. Please the link path to your repository`;
+				sendErrorMessage(message, res, name, db);
 			}
-		} else {
-			// Directory not linked
-			const message = `No local path found for ${payload.name}. Please the link path to your repository`;
-			sendErrorMessage(message, res, payload.name, db);
+		} catch (err) {
+			message = `Error! Not a valid status event. ${JSON.stringify(err)}`;
+			sendErrorMessage(message, res);
 		}
 	} else {
-		message = 'Not a valid state event. Ignored';
+		message = 'Not a valid event. Ignored';
 		sendErrorMessage(message, res);
 	}
 };
